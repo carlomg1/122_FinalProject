bd7d67324cd1fb36729a3a83167124851e8d34b9
Zhaomin Li <zhaominl@uci.edu>
2019-03-09 11:07:07 -0800

    add all files for LOGIC pack

---
+++ logic/MemoryLogic.java
@@ -0,0 +1,28 @@
+package Logic;
+
+import javax.swing.*;
+
+public class MemoryLogic implements GameLogic {
+	
+	private int CompleteCount;
+	
+	@Override
+	public String getButtonClicked(){
+		
+	}
+	
+	
+	@Override
+	public boolean checkValidMove (JButton button){
+		
+	}
+	
+	public boolean checkFinish(){
+		
+	}
+	
+	public boolean onRonudCheck(){
+		
+	}
+
+}
---
+++ logic/OthelloGameState.java
@@ -0,0 +1,15 @@
+package Logic;
+
+import java.util.ArrayList;
+
+public class OthelloGameState{
+	
+	private String playerTurn;
+	private int score;
+	private ArrayList<ArrayList<Integer>> playerOneCurrentState;
+	private ArrayList<ArrayList<Integer>> playerTwoCurrentState;
+			
+	public void update(){}
+	public void changeTurn(){}
+	public void gameLoop(){}
+}
