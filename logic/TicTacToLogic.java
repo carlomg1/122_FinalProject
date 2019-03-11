f5bd144a6ad9d7d69a9cdea08904ddfb892442ee
garfieldluva3 <stephaniephung12@gmail.com>
2019-03-11 14:30:52 -0700

    Added in TicTacTo Game

--- logic/TicTacToLogic.java
+++ logic/TicTacToLogic.java
@@ -1,30 +1,173 @@
-package Logic;
+package logic;
 
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class TicTacToLogic implements GameLogic {
 	
-	private String turn;
+	TicTacToGameState tictactoGameState= new TicTacToGameState();
+	static int numberButtons=9;
+	private static JButton buttons[] = new JButton[numberButtons]; 
+	public String turn;
+	int counterSwitchPlayer=0;
 	
-	@Override
-	public String getButtonClicked(){
-		return turn;
-		
+	private static int[][] winCombinations = new int[][] {
+		{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //horizontal wins
+		{0, 3, 6}, {1, 4, 7}, {2, 5, 8}, //vertical wins
+		{0, 4, 8}, {2, 4, 6}			 //diagonal wins
+};
+	
+	
+	
+	public boolean checkValidMove (buttonListener button){
+		System.out.println("HERE"+button.checkDiagonalWin());
+		if (button.checkDiagonalWin()==true) {
+		return true;
 	}
-	
-	
-	@Override
-	public boolean checkValidMove (JButton button){
 		return false;
-		
 	}
 	
-	public boolean findWinner(){
-		return false;
-		
+	 private class buttonListener extends JButton implements ActionListener
+	    {
+		 
+		 public buttonListener() {
+			 addActionListener(this);// allows button to be clickable and start performing the actions on it
+		 	}
+		 
+	        public void actionPerformed(ActionEvent action) 
+	        {
+	           changeTurn(action);
+	            findWinner(action);
+	        }
+	        
+	        
+	    	public String findWinner(ActionEvent action){
+	    		if(checkDiagonalWin()  == true || checkVerticalWin()==true || checkHorizontalWin()==true)
+	            {
+					int answer=JOptionPane.showConfirmDialog(null, turn + " wins the game!  Do you want to play again?","",JOptionPane.YES_NO_OPTION);
+	                if(answer==JOptionPane.NO_OPTION){
+	    				System.exit(0);}
+	                
+	                if(answer==JOptionPane.YES_OPTION) { // if the user want to play again clear all the button and start over
+	                	resetButtons();
+				}
+	                //resetButtons();
+	            }   
+	            counterSwitchPlayer++;
+	            return turn;
+	    	}
+	        
+	        
+	        
+	        public void changeTurn(ActionEvent buttonPress) {
+	        	JButton buttonClicked=(JButton)buttonPress.getSource();
+	        	if (counterSwitchPlayer %2==0) {
+	        		buttonClicked.setText("X");
+	        		turn="Player 1";
+	        	}
+	        	
+	        	else {
+	        		buttonClicked.setText("O");
+	        		turn="Player 2";
+	        		}
+	        }
+	        
+	        
+	        public boolean checkDiagonalWin() {
+	        	if (findWinner(0,4) && findWinner(4,8) || findWinner(2,4) && findWinner(4,6))
+	                return true;  
+	            else {
+	                return false;
+	        }
+	        }
+	        
+	        public boolean checkVerticalWin(){
+	        	if (findWinner(0,3) && findWinner(3,6)
+	        			|| findWinner(1,4) && findWinner(4,7) 
+	        			|| findWinner(2,5) && findWinner(5,8)) {
+	        		 return true; 
+	        	}
+
+	        	else
+	        		return false;
+	        	
+	        }
+	        
+	        
+	        public boolean checkHorizontalWin() {
+	        	if (findWinner(0,1) && findWinner(1,2) 
+	        			|| findWinner(3,4) && findWinner(4,5) 
+	        			|| findWinner(6,7) && findWinner(7,8)) {
+	        		 return true;
+	        	}
+	        	else {
+	        return false;
+	        }
+	        }
+	        
+	        
+	        public boolean findWinner(int row,int column)
+	        {
+	            if (buttons[row].getText().equals(buttons[column].getText()) && !buttons[row].getText().equals(""))
+	                return true;
+	            else
+	                return false;
+	        }
+	        
+	    }
+	
+
+    public void resetButtons()
+    {
+        for(int i = 0; i <= numberButtons-1; i++)// i will be row times columns minus 1
+        {
+            buttons[i].setText("");
+        }
+    }
+
+
+	public void initializeBoard() {
+		JFrame frame = new JFrame ("Tic Tac Toe");
+		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+
+		JPanel panel = new JPanel(); //creating a panel with a box like a tic tac toe board
+		panel.setLayout (new GridLayout (TicTacToGameState.rows, TicTacToGameState.columns));	
+		for(int i=0; i<=numberButtons-1; i++){ //placing the button onto the board
+			buttons[i] = new buttonListener();
+			checkValidMove(buttons[i]);
+			System.out.println("PASSES HERE"+checkValidMove(buttons[i]));
+			panel.add(buttons[i]);	
+			
+			 
+		}
+		frame.getContentPane().add (panel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+
+
 	}
-	public void changeTurn(){
-		
+    public static void main (String[] args) 
+    {
+    	TicTacToLogic tictactologic=new TicTacToLogic();
+    	tictactologic.initializeBoard();
+
+    }
+
+
+	@Override 
+	public boolean checkValidMove(JButton button) {
+		buttonListener here=new buttonListener();
+	
+		return false;
 	}
 
 }
--- view/TicTacToeView.java
+++ view/TicTacToeView.java
@@ -1,18 +1,44 @@
-package View;
+package view;
+
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+import logic.TicTacToLogic;
+import java.awt.GridLayout;
+
+import javax.swing.*;
 
 public class TicTacToeView implements GameView {
 	public String Player1;
 	public String Player2;
 	public Board boardGame;
+	public JFrame jframe=new JFrame();
+	
+	
 	
 	public TicTacToeView() {
+		JPanel panel=new JPanel();
+		jframe.getContentPane().add(panel);
+		jframe.setLayout(null);
+		
+		jframe.setTitle("TicTacToe");
+		jframe.setSize(500, 500);
+		jframe.setLocationRelativeTo(null);
+		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
 		
 	}
 
 	@Override
 	public void startGame() {
-		// TODO Auto-generated method stub
+		TicTacToLogic games=new TicTacToLogic();
+		games.initializeBoard();
 		
+		//jframe.setVisible(true);
+
+//		boardGame.coordX=3;
+//		boardGame.coordY=3;
+//	    boardGame.setLayout(new GridLayout(3,3));
+	   
+
 	}
 
 	@Override
