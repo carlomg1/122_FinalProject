32186c65630297e5657b85eaae93c77674d65656
Zhaomin Li <zhaominl@uci.edu>
2019-03-11 17:22:57 -0700

    updateBoard working

--- view/CheckersView.java
+++ view/CheckersView.java
@@ -3,6 +3,7 @@ package View;
 import java.awt.*;  
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicButtonListener;
+import Logic.CheckersGameState;
 
 public class CheckersView implements GameView {
 	public String Player1;
@@ -11,8 +12,10 @@ public class CheckersView implements GameView {
 	public JFrame frame;
 	public JPanel panel;
 	public Button[][] buttons;
+	public CheckersGameState GameState;
 	
 	public CheckersView() {
+		GameState = new CheckersGameState();
 		this.buttons = new Button[8][];
 		for (int i = 0; i<8; ++i){
 			this.buttons[i]=new Button[8];
@@ -38,35 +41,7 @@ public class CheckersView implements GameView {
 		this.panel.setBorder (BorderFactory.createLineBorder (Color.red, 8));
 		this.panel.setBackground (Color.white);
 		
-		boolean black;
-		for(int i=0; i<8; i++){ //placing the button onto the board
-			if ( i % 2 == 0){
-				black = true;
-			}else{
-				black = false;
-			}
-			for (int j =0; j<8; ++j){
-				if (black){
-					final JLabel label = new JLabel();
-					label.setBackground(Color.gray);
-					label.setForeground(Color.gray);
-					label.setOpaque(true);
-					label.setBorder(BorderFactory.createLineBorder(Color.gray));
-					this.panel.add(label);
-					black = false;
-				}else{
-					final JLabel label = new JLabel();
-					label.setBackground(Color.white);
-					label.setForeground(Color.white);
-					label.setOpaque(true);
-					label.setBorder(BorderFactory.createLineBorder(Color.gray));
-					this.panel.add(label);
-					black = true;
-				}
-				//buttons[i][j] = new Button();
-				//this.panel.add(buttons[i][j]);	
-			}
-		}
+		this.updateBoard();
 		this.frame.getContentPane().add(panel);
 		this.frame.pack();
 		this.frame.setVisible(true);
@@ -84,6 +59,103 @@ public class CheckersView implements GameView {
 	public void updateBoard() {
 		// TODO Auto-generated method stub
 		
+		boolean black;
+		for(int i=0; i<8; i++){ //placing the player 1 checkers
+			if ( i % 2 == 0){
+				black = false;
+			}else{
+				black = true;
+			}
+			for (int j =0; j<8; ++j){ 
+				if (GameState.playerOneCurrentState[i][j]==1){
+					final JButton button = new JButton(new CircleIconBlack());
+					button.setBackground(Color.gray);
+					button.setForeground(Color.gray);
+					button.setOpaque(true);
+					button.setBorder(BorderFactory.createLineBorder(Color.gray));
+					this.panel.add(button);
+					black = false;
+				}
+				else if (GameState.playerTwoCurrentState[i][j]==1){
+					final JButton button = new JButton(new CircleIconRed());
+					button.setBackground(Color.gray);
+					button.setForeground(Color.gray);
+					button.setOpaque(true);
+					button.setBorder(BorderFactory.createLineBorder(Color.gray));
+					this.panel.add(button);
+					black = false;
+				}
+				else
+				{
+					if (black){
+						final JButton button = new JButton();
+						button.setBackground(Color.gray);
+						button.setForeground(Color.gray);
+						button.setOpaque(true);
+						button.setBorder(BorderFactory.createLineBorder(Color.gray));
+						this.panel.add(button);
+						black = false;
+					}else{
+						final JLabel label = new JLabel();
+						label.setBackground(Color.white);
+						label.setForeground(Color.white);
+						label.setOpaque(true);
+						label.setBorder(BorderFactory.createLineBorder(Color.gray));
+						this.panel.add(label);
+						black = true;
+					}
+				}
+			}
+		}
 	}
+	
+	class CircleIconBlack implements Icon {
+		  @Override
+		  public void paintIcon(Component c, Graphics g, int x, int y) {
+			  
+		    Graphics2D g2 = (Graphics2D) g.create();
+		    //Draw the icon at the specified x, y location:
+		    g2.drawOval(8, 8, c.getWidth()-16, c.getHeight()-16);
+		    
+		    g2.setColor(Color.black);
+		    g2.setBackground(Color.gray);
+		    
+		    g2.fillOval(8, 8, c.getWidth()-16, c.getHeight()-16);
+		    g2.dispose();
+		  }
 
+		  @Override
+		  public int getIconWidth() {
+		    return 30;
+		  }
+		  @Override
+		  public int getIconHeight() {
+		    return 30;
+		  }
+		}
+	
+	class CircleIconRed implements Icon {
+		  @Override
+		  public void paintIcon(Component c, Graphics g, int x, int y) {
+		    //g.drawOval(10, 10, 20, 20);
+		    Graphics2D g2 = (Graphics2D) g.create();
+		    //Draw the icon at the specified x, y location:
+		    //g2.drawOval(1, 1, c.getWidth()-3, c.getHeight()-3);
+		    g2.setColor(Color.red);
+		    g2.fillOval(8, 8, c.getWidth()-16, c.getHeight()-16);
+		    //or
+		    //g2.translate(x, y);
+		    //g2.drawOval(0, 0, getIconWidth() - 1, getIconHeight() - 1);
+		    g2.dispose();
+		  }
+
+		  @Override
+		  public int getIconWidth() {
+		    return 30;
+		  }
+		  @Override
+		  public int getIconHeight() {
+		    return 30;
+		  }
+		}
 }
