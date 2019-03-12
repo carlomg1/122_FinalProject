d686e7c897b259e552c3badb39773012d27a0048
Armando Torres <armant1@uci.edu>
2019-03-12 15:38:07 -0700

    Board Made

--- view/OthelloView.java
+++ view/OthelloView.java
@@ -2,7 +2,10 @@ package view;
 
 import javax.swing.JPanel;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -11,10 +14,15 @@ public class OthelloView implements GameView {
 	public String Player2;
 	public Board boardGame;
 	public JFrame jframe = new JFrame();
+	public JPanel panel;
+	public int numberButtons;
+	public JButton buttons[][];
 	
 	public OthelloView() {
 		//Set JPanel
-		JPanel panel = new JPanel();
+		panel = new JPanel();
+		panel.setBorder (BorderFactory.createLineBorder (Color.black, 8));
+		panel.setBackground (Color.GREEN);
 		jframe.getContentPane().add(panel);
 		panel.setLayout(null);
 		
@@ -23,19 +31,35 @@ public class OthelloView implements GameView {
 		jframe.setSize(500,500);
 		jframe.setLocationRelativeTo(null);
 		jframe.setDefaultCloseOperation(jframe.EXIT_ON_CLOSE);
+		
+		//Initialize Button Array
+		numberButtons=64;
+		buttons = new JButton[8][8]; 
 	}
 
 	@Override
 	public void startGame() {
 		// TODO Auto-generated method stub
+		layoutGrid();
+		populateStartGrid();
 		jframe.setVisible(true);
 		
 	}
 
 	@Override
 	public void layoutGrid() {
-		// TODO Auto-generated method stub
-		
+		panel.setLayout (new GridLayout (8, 8));	
+		for(int i = 0 ; i < 8 ; i++){
+			for(int j = 0 ; j < 8 ; j++) {
+				JButton button = new JButton();
+				buttons[i][j] = button;
+				button.setBackground(Color.green);
+				button.setForeground(Color.black);
+				button.setOpaque(true);
+				button.setBorder(BorderFactory.createLineBorder(Color.black));
+				panel.add(button);
+			}
+		}
 	}
 
 	@Override
@@ -49,5 +73,20 @@ public class OthelloView implements GameView {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	private class buttonListener extends JButton implements ActionListener
+    {
+	 
+	 public buttonListener() {
+		 addActionListener(this);// allows button to be clickable and start performing the actions on it
+	 	}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+        
+    }
 
 }
