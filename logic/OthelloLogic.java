42f10989e6fdc2eb6463a9645cdc980e9cd655ff
Armando Torres <armant1@uci.edu>
2019-03-14 16:07:19 -0700

    fixed merge from master into armando

--- logic/OthelloLogic.java
+++ logic/OthelloLogic.java
@@ -6,37 +6,142 @@ import view.MainGUI;
 
 public class OthelloLogic implements GameLogic {
 	public OthelloGameState othelloGameState = new OthelloGameState();
-	
+	private int black;
+	private int white;
+
 	public OthelloLogic() {
+		black = othelloGameState.black;
+		white = othelloGameState.white;
+		othelloGameState.playerTurn = black;
 		initializeBoard();
 	}
-	
+
 	@Override
 	public boolean checkValidMove (JButton button){
 		return false;
-		
+
 	}
-	
+
 	public boolean findWinner(){
 		return false;
-		
+
 	}
 	public void changeTurn(){
-		
+
 	}
-	
+
 	public void initializeBoard() {
-		othelloGameState.board[3][3] = othelloGameState.black;
-		othelloGameState.board[3][4] = othelloGameState.white;
-		othelloGameState.board[4][3] = othelloGameState.white;
-		othelloGameState.board[4][4] = othelloGameState.black;
-		
-		for(int row = 0 ; row < othelloGameState.board.length ; row++) {
-			System.out.println();
-			for(int j = 0 ; j < othelloGameState.board.length ; j++) {
-				System.out.print(othelloGameState.board[row][j]);
+		othelloGameState.board[3][3] = black;
+		othelloGameState.board[3][4] = white;
+		othelloGameState.board[4][3] = white;
+		othelloGameState.board[4][4] = black;
+	}
+
+	public OthelloGameState handleMove(int x, int y) {
+		if(isValidMove(x,y)) {
+			switchTurns();
+			return othelloGameState;
+		}
+		else {
+			return null;
+		}
+	}
+
+	private int getOppositeTurn(int currentTurn) {
+		if(currentTurn == 1) {
+			return 2;
+		}
+		else {
+			return 1;
+		}
+	}
+
+	private boolean checkUpper(int x, int y) {
+		try {
+			int oppositeTurn = getOppositeTurn(othelloGameState.playerTurn);
+			if(othelloGameState.board[x - 1][y] == oppositeTurn) {
+				System.out.println("Valid");
+				return true;
+			}
+			else {
+				return false;
 			}
 		}
+		catch(Exception ArrayIndexOutOfBoundsException) {
+
+		}
+		return false;
+
+	}
+
+	private boolean checkLower(int x, int y) {
+		try {
+			int oppositeTurn = getOppositeTurn(othelloGameState.playerTurn);
+			if(othelloGameState.board[x+1][y] == oppositeTurn) {
+				System.out.println("Valid");
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		catch(Exception ArrayIndexOutOfBoundsException) {
+
+		}
+		return false;
+
+	}
+
+	private boolean checkLeft(int x, int y) {
+		try {
+			int oppositeTurn = getOppositeTurn(othelloGameState.playerTurn);
+			if(othelloGameState.board[x][y-1] == oppositeTurn) {
+				System.out.println("Valid");
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		catch(Exception ArrayIndexOutOfBoundsException) {
+
+		}
+		return false;
+
+	}
+
+	private boolean checkRight(int x, int y) {
+		try {
+			int oppositeTurn = getOppositeTurn(othelloGameState.playerTurn);
+			if(othelloGameState.board[x][y+1] == oppositeTurn) {
+				System.out.println("Valid");
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		catch(Exception ArrayIndexOutOfBoundsException) {
+
+		}
+		return false;
+
+	}
+
+	private boolean isValidMove(int x, int y) {
+		if(othelloGameState.board[x][y] == 0 && (checkUpper(x,y)
+				|| checkLower(x,y)
+				|| checkRight(x,y)
+				|| checkLeft(x,y))) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	private void switchTurns() {
+		othelloGameState.playerTurn = getOppositeTurn(othelloGameState.playerTurn);
 	}
 
 }
