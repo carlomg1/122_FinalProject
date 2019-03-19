b122707e84313be32dad0121b15bd61ef06ab5e2
Seong Ma <sjma1@uci.edu>
2019-03-19 13:42:26 -0700

    Changed TicTacTo stuff

--- view/TicTacToeView.java
+++ view/TicTacToeView.java
@@ -1,11 +1,12 @@
-
 package view;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import logic.TicTacToGameState;
 import logic.TicTacToLogic;
-import logic.TicTacToLogic.buttonListener;
+import logic.TicTacToLogic.TicTacToButton;
 
 import java.awt.GridLayout;
 
@@ -17,10 +18,12 @@ public class TicTacToeView implements GameView {
 	public Board boardGame;
 	public JFrame frame;
 	public JPanel panel;
+	public JPanel scoreboard;
 	public TicTacToGameState GameState;
 	public TicTacToLogic tictactologic;
 	static int numberButtons=9;
 	public static JButton buttons[] = new JButton[numberButtons];
+	public JLabel score;
 
 
 // goes from startGame is the main, then it will first call layoutGrid and then updateBoard
@@ -28,34 +31,58 @@ public class TicTacToeView implements GameView {
 
 	@Override
 	public void startGame() {
-		this.layoutGrid();
+		this.populateStartGrid();
 	}
+	
 
 	@Override
 	public void layoutGrid() {
 		this.frame = new JFrame ("Tic Tac Toe");
-		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.updateBoard();
+		this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		//this.updateBoard();
 	}
 	@Override
 	public void updateBoard() {
 		this.panel=new JPanel();
 		this.panel.setLayout(new GridLayout(3,3));
+		this.panel.setBorder(BorderFactory.createLineBorder (Color.black, 8));
+		this.panel.setBackground(Color.white);
+		
+		this.scoreboard=new JPanel();
+		this.scoreboard.setLayout(new FlowLayout());
+		this.scoreboard.setBorder(BorderFactory.createLineBorder (Color.black, 8));
+		this.scoreboard.setBackground(Color.white);
+		
+		JSplitPane splitPane=new JSplitPane();
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPane.setDividerLocation(100);
+		splitPane.setTopComponent(this.scoreboard);
+		splitPane.setBottomComponent(this.panel);
+		this.score=new JLabel();
+		this.score.setText("Player 1:" + MainGUI.username1   +"  Player 2: " + MainGUI.username2 );
+		this.scoreboard.add(score);
+		
+		this.frame.getContentPane().add(splitPane);		
+		
+		
 		for(int i=0; i<=numberButtons-1; i++){ //placing the button onto the board
-			buttons[i] = new buttonListener();
-			System.out.println("HERE"+buttons[i]);
+			buttons[i] = new TicTacToButton();
 			this.panel.add(buttons[i]);
 		}
-		this.frame.getContentPane().add (this.panel);
-		this.frame.pack();
+		//this.frame.getContentPane().add (this.panel);
+		//this.frame.pack();
 		this.frame.setVisible(true);
 		this.frame.setSize(500, 500);
 	}
-
+  
+	@Override
 	public void populateStartGrid() {
-		// TODO Auto-generated method stub
-		//maybe dont need thisp
+		this.layoutGrid();
+		this.updateBoard();
+
 	}
+	
+
 
 
 }
